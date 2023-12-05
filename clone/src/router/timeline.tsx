e3dfec02c836348de1a579b1e2f7c2f022c4ffb5
hotteokButler <ygs12567@gmail.com>
2023-12-05 17:12:05 +0900

    Add :use onSnapshot
    ㄴ unmount될 때 snpashot 연결 해제 까지

--- clone/src/router/timeline.tsx
+++ clone/src/router/timeline.tsx
@@ -1,4 +1,4 @@
-import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import { collection, getDocs, onSnapshot, orderBy, query } from 'firebase/firestore';
 import { TimelineWrap } from '../components/styled';
 import { useState, useEffect } from 'react';
 import { db } from '../app/firebase_init';
@@ -8,25 +8,39 @@ import Tweet from '../components/tweet';
 function Timeline() {
   const [tweets, setTweet] = useState<ITweet[] | undefined>([]);
 
-  const fetchTweets = async () => {
-    const tweetQuery = query(collection(db, 'tweet'), orderBy('createdAt'));
-    const snapshot = await getDocs(tweetQuery);
-    const tweets = snapshot.docs.map((doc) => {
-      const { createdAt, photo, tweet, user_id, user_name } = doc.data();
-      return {
-        createdAt,
-        photo,
-        tweet,
-        user_id,
-        user_name,
-        id: doc.id,
-      };
-    });
-    setTweet(tweets);
-  };
+  const tweetQuery = query(collection(db, 'tweet'), orderBy('createdAt'));
+  // const fetchTweets = async () => {
+  //   const snapshot = await getDocs(tweetQuery);
+  //   const tweets = snapshot.docs.map((doc) => {
+  //     const { createdAt, photo, tweet, user_id, user_name } = doc.data();
+  //     return {
+  //       createdAt,
+  //       photo,
+  //       tweet,
+  //       user_id,
+  //       user_name,
+  //       id: doc.id,
+  //     };
+  //   });
+  // };
 
   useEffect(() => {
-    fetchTweets();
+    const unsubscribe = onSnapshot(tweetQuery, (snapshot) => {
+      const tweets = snapshot.docs.map((doc) => {
+        const { createdAt, photo, tweet, user_id, user_name } = doc.data();
+        return {
+          createdAt,
+          photo,
+          tweet,
+          user_id,
+          user_name,
+          id: doc.id,
+        };
+      });
+      setTweet(tweets);
+    });
+
+    return () => unsubscribe();
   }, []);
 
   return (
